06cd4f51be679cefb5790f29b94411ae6ac2a8ad
Amaan63 <amaansayed63@gmail.com>
2025-03-15 18:18:27 +0530

    Changed footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full bg-gray-900 text-white text-center py-4 mt-6">
+    <footer className="w-screen bg-red-600 text-white text-center py-4 mt-6">
       <p className="text-sm">
         © {new Date().getFullYear()} Amaan Sayyed. All rights reserved.
       </p>
