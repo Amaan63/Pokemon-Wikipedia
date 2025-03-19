ad81a651acc9e6c1f583a7ea476e3e266c332be0
Amaan63 <amaansayed63@gmail.com>
2025-03-19 22:33:30 +0530

    Changed the width

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-screen bg-red-600 text-white text-center py-4 mt-6">
+    // <footer className="w-screen bg-red-600 text-white text-center py-4 mt-6">
+    <footer className="w-11/12 bg-red-600 text-white text-center py-4 mt-6 mx-6">
       <p className="text-sm">
         © {new Date().getFullYear()} Amaan Sayyed. All rights reserved.
       </p>
