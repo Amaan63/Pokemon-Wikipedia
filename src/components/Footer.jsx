be5093501cfa4334f08e3c98eb059e05d1f17f1c
Amaan63 <amaansayed63@gmail.com>
2025-03-20 20:38:16 +0530

    Some styling changes in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     // <footer className="w-screen bg-red-600 text-white text-center py-4 mt-6">
-    <footer className="w-11/12 bg-red-600 text-white text-center py-4 mt-6 mx-6">
-      <p className="text-sm">
+    <footer className="w-11/12 bg-red-600 text-white text-center py-4 mt-6 mx-6 rounded-2xl">
+      <p className="text-sm font-semibold">
         © {new Date().getFullYear()} Amaan Sayyed. All rights reserved.
       </p>
     </footer>
